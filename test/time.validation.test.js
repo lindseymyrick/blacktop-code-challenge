c54d4722100d011a207982897a816f41b7b2b37a
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-07-13 15:31:55 -0500

    closeToNow test: time ten years ago

--- test/time.validation.test.js
+++ test/time.validation.test.js
@@ -12,24 +12,29 @@ describe('time validation', () => {
             expect(closeToNow(timestamp)).toBe(true);
             done();
         });
-        // YOUR CODE HERE
-    });
-    describe('closest date', () => {
-        test('it returns the previous day if before noon CST', (done) => {
-            const inputTime = '2020-07-01T16:03:18.021Z'
-            const expectedOutput = '2020-06-30';
-            expect(closestDate(inputTime)).toBe(expectedOutput);
+        test('it returns false if passed a time ten years ago', (done) => {
+            const timestamp = '2010-07-01T16:03:18.021Z'; 
+            expect(closeToNow(timestamp)).toBe(false);
             done();
         });
-        // YOUR CODE HERE
+       
     });
-    describe('format timestamp', () => {
-        test('it returns a formatted timestamp', (done) => {
-            const inputTime = '2020-07-01T16:03:18.021Z'
-            const expectedOutput = 'July 1st, 2020 at 11:03 am';
-            expect(formatTimestamp(inputTime, 'US/Central')).toBe(expectedOutput);
-            done();
-        });
-        // YOUR CODE HERE
-    });
+    // describe('closest date', () => {
+    //     test('it returns the previous day if before noon CST', (done) => {
+    //         const inputTime = '2020-07-01T16:03:18.021Z'
+    //         const expectedOutput = '2020-06-30';
+    //         expect(closestDate(inputTime)).toBe(expectedOutput);
+    //         done();
+    //     });
+    //     // YOUR CODE HERE
+    // });
+    // describe('format timestamp', () => {
+    //     test('it returns a formatted timestamp', (done) => {
+    //         const inputTime = '2020-07-01T16:03:18.021Z'
+    //         const expectedOutput = 'July 1st, 2020 at 11:03 am';
+    //         expect(formatTimestamp(inputTime, 'US/Central')).toBe(expectedOutput);
+    //         done();
+    //     });
+    //     // YOUR CODE HERE
+    // });
 });
