2ccd424bd531e6c3d60a4c52740f93ac7d173ac9
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-07-13 15:36:13 -0500

    closeToNow test: time 40 seconds  ago

--- test/time.validation.test.js
+++ test/time.validation.test.js
@@ -4,6 +4,9 @@ const {
     formatTimestamp,
 } = require('../modules/time.validation');
 
+const moment = require('moment-timezone');
+
+
 describe('time validation', () => {
     describe('close to now', () => {
         test('it returns true if passed the current time', (done) => {
@@ -14,6 +17,11 @@ describe('time validation', () => {
         });
         test('it returns false if passed a time ten years ago', (done) => {
             const timestamp = '2010-07-01T16:03:18.021Z'; 
+            expect(closeToNow(timestamp)).toBe(false);
+            done();
+        });
+        test('it returns false if passed a time 40 seconds ago', (done) => {
+            const timestamp = moment().subtract(40, 's');
             expect(closeToNow(timestamp)).toBe(false);
             done();
         });
